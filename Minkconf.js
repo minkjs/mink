a95c1d2e31b98dbae943ce26e8195d3796f3d241
F1LT3R <al@pwn.io>
2017-07-17 23:55:53 -0400

    Attach logger to service being passed to handler.

--- Minkconf.js
+++ Minkconf.js
@@ -5,6 +5,10 @@ const Minkconf = {
 			module: 'lib/plugins/markdown/mink-markdown'
 		},
 		{
+			path: '**/*.html',
+			module: 'lib/plugins/html/mink-html'
+		},
+		{
 			path: '**/',
 			module: 'lib/plugins/dir/mink-dir',
 			template: 'lib/plugins/dir/mink-dir.html'
@@ -13,6 +17,10 @@ const Minkconf = {
 			module: 'lib/plugins/fibonacci/mink-fibonacci',
 			tag: 'fibonacci'
 		},
+
+		// Catch-all for "other" content
+		// Note: mink-file uses pipes files to your browser
+		// without filtering content for <!--mink--> tags
 		{
 			path: '**/*',
 			module: 'lib/plugins/file/mink-file'
