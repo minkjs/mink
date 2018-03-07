3bd140c47513781041315b7502d8fc5c97eeaa03
F1LT3R <al@pwn.io>
2018-03-07 13:23:29 -0500

    Add test for Requests.requestHandler()

--- mock/config.log.js
+++ mock/config.log.js
@@ -1,14 +1,23 @@
+const path = require('path')
+
 const Log = require('app/lib/core/log')
 
-const cli = {
-	flags: {
-		logLevel: 'TRACE',
-		logColor: true,
-		systemUnderTest: true
+const MinkconfRoot = path.resolve(path.join(__dirname, '../'))
+
+module.exports = () => {
+	const cli = {
+		flags: {
+			logLevel: 'TRACE',
+			logColor: true,
+			systemUnderTest: true
+		}
 	}
+
+	const log = Log(cli)
+	const config = {
+		MinkconfRoot,
+		log
+	}
+
+	return config
 }
-
-const log = Log(cli)
-const config = {log}
-
-module.exports = config
