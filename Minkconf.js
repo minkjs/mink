20feb5d3734019f80a651d939ea916365674474b
F1LT3R <al@pwn.io>
2018-03-14 13:06:51 -0400

    #18 - Add magic handler params, Minkconf handlers, and  params like .

--- Minkconf.js
+++ Minkconf.js
@@ -1,27 +1,49 @@
+const ts = require('typescript')
+
 const Minkconf = {
-	plugins: [
-		{
+	data: {
+		foo: 'bar'
+	},
+	plugins: {
+		markdown: {
 			path: '**/*.md',
-			module: './lib/plugins/markdown/mink-markdown'
+			handler: './lib/plugins/markdown/mink-markdown'
 		},
-		{
-			path: '**/*.html',
-			module: './lib/plugins/html/mink-html'
-		},
-		{
-			path: '**/',
-			module: 'lib/plugins/dir/mink-dir',
-			template: 'lib/plugins/dir/mink-dir.html'
-		},
+		typescript: {
+			path: '**/*.ts',
+			handler: ($uri, $res, $data, $file) => {
+				// Log all the things
+				// console.log($uri)
+				// console.log($data)
+				// console.log($file)
+				const transpiled = ts.transpileModule(String($file.contents), {}).outputText
+				$res.end(`uri: ${$uri}, data: ${$data}, ts: ${transpiled}`)
+			}
+		}
+
+		// Save for later
+		// {
+		// 	path: '**/*.html',
+		// 	module: './lib/plugins/html/mink-html'
+		// },
+		// {
+		// 	path: '**/',
+		// 	module: 'lib/plugins/dir/mink-dir',
+		// 	template: 'lib/plugins/dir/mink-dir.html'
+		// },
 
 		// Catch-all for "other" content
 		// Note: mink-file uses pipes files to your browser
 		// without filtering content for <!--mink--> tags
-		{
-			path: '**/*',
-			module: 'lib/plugins/file/mink-file'
-		}
-	]
+		// {
+		// 	path: '**/*',
+		// 	module: 'lib/plugins/file/mink-file'
+		// }
+		// {
+		// 	path: '**/*',
+		// 	module: 'docs/plugin-example-01.js'
+		// }
+	}
 }
 
 module.exports = Minkconf
